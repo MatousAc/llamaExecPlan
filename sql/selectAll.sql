3f279a9c2367a6751a3862155212061fda9d0f21
MatousAc <matoush@southern.edu>
2024-03-04 15:15:37 -0500

    add sql queries and better schema

---
+++ sql/selectAll.sql
@@ -0,0 +1,28 @@
+use attendanceDB;
+
+-- 2067
+select * from student;
+-- 2
+select * from term;
+-- 3451
+select * from studentTerm;
+-- 135
+select * from major;
+-- 4696
+select * from studentMajorTerm;
+-- 221600
+select * from attendance;
+-- 7932
+select * from event;
+-- 17
+select * from room;
+-- 63
+select * from professor;
+-- 161
+select * from section;
+-- 6999
+select * from studentSection;
+-- 13
+select * from department;
+-- 79
+select * from course;
